3271d6c5dd8cf06eee57083c3bd710ac3a315662
Regbmiller <40448656+Regbmiller@users.noreply.github.com>
2020-01-30 19:27:56 -0600

    login form

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import {loginInfo} from '../data/data'
 
 export default function Login() {
+
+    const [logindata, setLoginData] = usestate(
+        {
+            "username": "", 
+            "Password": "", 
+        }
+    )
+    
+    const handleChange = e => {
+        return setLoginData({...logindata, [e.target.name]: e.target.value}, )
+    } 
+
+    useEffect(() => {
+        axios.post('api', logindata)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
-            
+          <form>
+        <label htmlFor='username'>Username</label>
+           <input 
+            placeholder='User Name'
+            id='usernameinput'
+            name='Username'
+            type= 'text'
+            value=''
+           />
+          </form>
+          <form>
+        <label htmlFor='username'>Password</label>
+           <input 
+            placeholder='User Name'
+            id='passwordinput'
+            name='Password'
+            type= 'text'
+            value=''
+           />
+
+           <button type="submit">Submit</button>
+          </form>
         </div>
     )
 }
+
+
