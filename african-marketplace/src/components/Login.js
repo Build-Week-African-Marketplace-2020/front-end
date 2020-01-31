d31ca7693bc5d89cf917801019ca5a46dc315bba
Leon Chisum <leonchisum3306@gmail.com>
2020-01-30 22:18:33 -0500

    sign up form function complete

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
+import React from 'react'
 import axios from 'axios'
 import {loginInfo} from '../data/data'
 
 export default function Login() {
 
-    const [logindata, setLoginData] = usestate(
+    const [logindata, setLoginData] = useState(
         {
             "username": "", 
             "Password": "", 
--- african-marketplace/src/components/SignUp.js
+++ african-marketplace/src/components/SignUp.js
@@ -5,10 +5,10 @@ export default function SignUp(props) {
 
     const [newUser, setNewUser] = useState(
         {
-            "username": "t",
-            "password": "t",
-            "first_name": "t",
-            "last_name": "t"
+            "username": "",
+            "password": "",
+            "first_name": "",
+            "last_name": ""
         }
     )
 
@@ -17,41 +17,58 @@ export default function SignUp(props) {
         console.log(newUser.username)
     }
 
-    useEffect(() => {
+    const handleSubmit = e => {
+        e.preventDefault();
         axios.post('https://africanmarketplacels.herokuapp.com/auth/register', newUser)
-             .then(res => {
-                console.log(res)
-                props.history.push('/login')
-                alert(`Login to Continue!`)
-             })
-             .catch(err => console.log(err))
-    }, [])
+        .then(res => {
+           console.log(res)
+           props.history.push('/login')
+           alert(`Login to Continue!`)
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
         <div>
            <div>
-          <form>
-        <label htmlFor='username'>Username</label>
-           <input 
-            placeholder='User Name'
-            id='usernameinput'
-            name='username'
-            type= 'text'
-            value={newUser.username}
-            onChange={handleChange}
-           />
-          </form>
-          <form>
-        <label htmlFor='username'>Password</label>
-           <input 
-            placeholder='User Name'
-            id='passwordinput'
-            name='Password'
-            type= 'text'
-           />
-           <button type="submit">Submit</button>
-          </form>
-        </div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='username'>Username</label>
+                <input 
+                    placeholder='User Name'
+                    id='usernameinput'
+                    name='username'
+                    type= 'text'
+                    value={newUser.username}
+                    onChange={handleChange}
+                />
+                <label>Password</label>
+                <input 
+                    placeholder='User Name'
+                    id='passwordinput'
+                    name='password'
+                    type= 'text'
+                    value={newUser.password}
+                    onChange={handleChange}
+                />
+                <label>First Name</label>
+                <input 
+                    placeholder='First Name'
+                    name='first_name'
+                    type= 'text'
+                    value={newUser.first_name}
+                    onChange={handleChange}
+                />
+                <label>Last Name</label>
+                <input 
+                    placeholder='Last Name'
+                    name='last_name'
+                    type= 'text'
+                    value={newUser.last_name}
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            </div>
         </div>
     )
 }
